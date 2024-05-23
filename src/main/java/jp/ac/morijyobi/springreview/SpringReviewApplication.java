01b15fa73e16b5df7eda81edfdcf2df61594cef3
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-05-23 12:12:44 +0900

    新規プロジェクト作成

---
+++ src/main/java/jp/ac/morijyobi/springreview/SpringReviewApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.springreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/ac/morijyobi/springreview/SpringReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.springreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
