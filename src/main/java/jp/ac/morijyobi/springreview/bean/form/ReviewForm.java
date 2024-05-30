a43e0056045c9512da9fb89bac611a5e71424910
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-05-30 12:26:03 +0900

    Formの作成

---
+++ src/main/java/jp/ac/morijyobi/springreview/bean/form/ReviewForm.java
@@ -0,0 +1,41 @@
+package jp.ac.morijyobi.springreview.bean.form;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ReviewForm {
+    @NotBlank
+    private String title;
+
+    private int rate;
+
+    @NotBlank
+    private String reviewTypeCode;
+
+    public ReviewForm() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getReviewTypeCode() {
+        return reviewTypeCode;
+    }
+
+    public void setReviewTypeCode(String reviewTypeCode) {
+        this.reviewTypeCode = reviewTypeCode;
+    }
+}
