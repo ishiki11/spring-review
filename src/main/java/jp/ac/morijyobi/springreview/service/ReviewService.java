83bf8bdffc1e2e03d85fa11a4bd2c286e2593e15
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2024-05-31 14:28:46 +0900

    List表示用のServiceファイル作成

--- src/main/java/jp/ac/morijyobi/springreview/service/ReviewService.java
+++ src/main/java/jp/ac/morijyobi/springreview/service/ReviewService.java
@@ -1,8 +1,13 @@
 package jp.ac.morijyobi.springreview.service;
 
+import jp.ac.morijyobi.springreview.bean.ReviewList;
 import jp.ac.morijyobi.springreview.bean.entity.Review;
 import jp.ac.morijyobi.springreview.bean.form.ReviewForm;
 
+import java.util.List;
+
 public interface ReviewService {
     Review registerReview(ReviewForm reviewForm);
+
+    List<ReviewList> getAllReview();
 }
